6bfe9b3ee179de467b86dcd3abb73f878f2a2dfe
raymondso123 <blotzytgaming@gmail.com>
2025-01-31 19:11:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -145,8 +145,8 @@ document.addEventListener("touchstart", (e) => {
         if (tile !== draggedTile) {
             tile.style.opacity = (tile.dataset.value === draggedTile.dataset.value) ? "1" : "0.3";
             if (tile.dataset.value == draggedTile.dataset.value) {
-                tile.style.boxShadow = "0px 0px 15px 5px rgba(0, 255, 98, 0.46)"; // offset, blur, spread, rgb
-                tile.style.border = "3px solid rgb(199, 255, 182)";
+                tile.style.boxShadow = "0px 0px 10px 2px rgba(0, 255, 98, 0.46)"; // offset, blur, spread, rgb
+                tile.style.border = "2px solid rgb(193, 243, 178)";
             }
         }
     }
